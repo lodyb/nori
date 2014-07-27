dcc7c1a34118127adde18fc8384bba5e971e3f92
Tomasz Jan Góralczyk <tomg@fastmail.uk>
2014-07-27 20:05:21 +0100

    Launch SettingsActivity from SearchActivity

---
+++ app/src/main/java/com/cuddlesoft/nori/SettingsActivity.java
@@ -0,0 +1,13 @@
+/*
+ * This file is part of nori.
+ * Copyright (c) 2014 Tomasz Jan Góralczyk <tomg@fastmail.uk>
+ * License: ISC
+ */
+
+package com.cuddlesoft.nori;
+
+import android.preference.PreferenceActivity;
+
+/** Main settings activity launched from {@link com.cuddlesoft.nori.SearchActivity}. */
+public class SettingsActivity extends PreferenceActivity {
+}

8144f6994d918b184667e820f9c3762ba8fb1c4e
Tomasz Jan Góralczyk <tomg@fastmail.uk>
2014-09-01 11:58:26 +0100

    Update copyright

--- app/src/androidTest/java/com/cuddlesoft/nori/test/database/APISettingsDatabaseTest.java
+++ app/src/androidTest/java/com/cuddlesoft/nori/test/database/APISettingsDatabaseTest.java
@@ -1,3 +1,9 @@
+/*
+ * This file is part of nori.
+ * Copyright (c) 2014 Tomasz Jan Góralczyk <tomg@fastmail.uk>
+ * License: ISC
+ */
+
 package com.cuddlesoft.nori.test.database;
 
 import android.content.BroadcastReceiver;
